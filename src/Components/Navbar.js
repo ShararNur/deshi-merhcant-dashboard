3cb7ab0a88faa631d62c348cce8170e12c7b93a4
Sharar Nur <shararnur@gmail.com>
2022-07-03 19:14:29 +0600

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="div">
+            <h1>Dashboard Page</h1>
+        </div>
+    );
+};
+
+export default Navbar;
